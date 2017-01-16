71cc3c1e48a84e55bf50e3cd83566a5c9e745e0c
Per Cederberg <per@percederberg.net>
2017-01-16 21:29:28 +0000

    test: Added initial unit test (for common library)

---
+++ test/test-common.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# Runs tests for mixtape-common library.
+#
+
+# Import assert functions
+source $(dirname $0)/assert.sh
+
+# Import common functions
+source $(dirname $0)/../bin/mixtape-common.sh
+
+# Tests the index_datetime function
+test_index_datetime() {
+    local ID="@587c2ac4"
+    local FILE="/backup/host/mixtape/index/index.2017-01-16-0207.txt.xz"
+    assert "index_datetime ${ID}" "2017-01-16 02:07"
+    assert "index_datetime ${ID} file" "2017-01-16-0207"
+    assert "index_datetime ${ID} %F" "2017-01-16"
+    assert "index_datetime ${FILE}" "2017-01-16 02:07"
+    assert "index_datetime ${FILE} file" "2017-01-16-0207"
+    assert "index_datetime ${FILE} %F" "2017-01-16"
+}
+
+# Tests the index_epoch function
+test_index_epoch() {
+    local FILE="/backup/host/mixtape/index/index.2017-01-16-0207.txt.xz"
+    assert "index_epoch ${FILE}" "@587c2ac4"
+}
+
+# Program start
+main() {
+    local FUNC
+    for FUNC in $(declare -F | cut -d ' ' -f 3 | grep ^test_) ; do
+        ${FUNC}
+    done
+    assert_end "mixtape-common"
+}
+
+main
