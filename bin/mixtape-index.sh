807e905d0f317c63eaa512e6959484ded3ed0e07
Per Cederberg <per@percederberg.net>
2017-01-17 16:30:17 +0000

    index: Support for listing content files
    Initially short and long formats are supported.

--- bin/mixtape-index.sh
+++ bin/mixtape-index.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 #
-# Searches for matching indices in the backup.
+# Lists indices in the backup, and optionally their content files.
 #
-# Syntax: mixtape-index [<id> | <pattern>]
+# Syntax: mixtape-index [--long] [<index> [<path>]]
 #
 # Arguments:
-#   <none>           Prints all available indices
-#   <id>             The unique index id (e.g. "@586efbc4") or the special
-#                    keywords "first" or "last"
-#   <pattern>        A partial timestamp (e.g. "2017-01") to search for
-#                    (accepts glob matches, i.e. ? and * chars)
+#   <index>          The optional index id (e.g. "@586efbc4"), named search
+#                    (e.g. "all", "first", "last") or partial timestamp (e.g.
+#                    "2017-01") with optional glob matching (e.g. "20??-*-01")
+#   <path>           The optional file path for listing index content. Use "/"
+#                    to show all files. An initial "/" char will be added if
+#                    missing.
+#
+# Options:
+#   --long           Prints content files in a longer format
 #
 
 # Import common functions
@@ -17,18 +21,78 @@ SCRIPT=$(readlink $0 || echo -n $0)
 LIBRARY=$(dirname ${SCRIPT})/mixtape-common.sh
 source ${LIBRARY} || exit 1
 
+# Prints index information
+index_info() {
+    local INDEX="$1"
+    echo -n "${COLOR_WARN}$(index_epoch ${INDEX})${COLOR_RESET}: ${INDEX}"
+    xzcat ${INDEX} | wc -l | awk '{printf " (%s entries",$1}'
+    du -h ${INDEX} | awk '{printf ", %s)\n",$1}'
+}
+
+# Reads index entries from stdin and prints them
+index_content_print() {
+    local FORMAT=${1:-short} COUNT=0 SIZE SIZEMB EXTRA
+    local INDEX ACCESS USER GROUP DATETIME SIZEKB FILE SHA LOCATION
+    while IFS=$'\t' read INDEX ACCESS USER GROUP DATETIME SIZEKB FILE SHA LOCATION ; do
+        SIZE=""
+        EXTRA=""
+        if [[ ${ACCESS:0:1} == "-" ]] ; then
+            if [[ ${SIZEKB} -gt 1024 ]] ; then
+                SIZEMB=$(echo ${SIZEKB} | awk '{printf "%.1f",$1/1024}')
+                SIZE="${SIZEMB}M"
+            else
+                SIZE="${SIZEKB}K"
+            fi
+        elif [[ ${ACCESS:0:1} == "l" ]] ; then
+            EXTRA=" -> ${LOCATION}"
+        fi
+        if [[ ${FORMAT} == "long" ]] ; then
+            printf "%s  %s  %6s  %s%s\n" ${ACCESS} "${DATETIME}" "${SIZE}" "${FILE}" "${EXTRA}"
+        #elif [[ ${FORMAT} == "full" ]] ; then
+        #    printf "%s  %s %s  %s  %6s  %s%s\n" ${ACCESS} ${USER} ${GROUP} "${DATETIME}" "${SIZE}" "${FILE}" "${EXTRA}"
+        else
+            printf "%s\n" "${FILE}"
+        fi
+        COUNT=$((COUNT+1))
+    done
+    if [[ ${COUNT} -eq 0 ]] ; then
+        printf -- "-- No matched files --\n"
+    else
+        printf -- "-- Total: ${COUNT} files --\n"
+    fi
+}
+
 # Program start
 main() {
-    local SEARCH="" INDEX
-    [[ ${#PROGARGS[@]} -le 1 ]] || usage
-    if [[ ${#PROGARGS[@]} -eq 1 ]] ; then
-        SEARCH="${PROGARGS[@]}"
+    local ID="" PREFIX="" FORMAT="short" INDEX FIRST=true
+    [[ ${#PROGARGS[@]} -le 2 ]] || usage
+    while [[ ${#PROGOPTS[@]} -gt 0 ]] ; do
+        case "${PROGOPTS[0]}" in
+        --long)
+            FORMAT="long"
+            ;;
+        *)
+            usage
+            ;;
+        esac
+        PROGOPTS=("${PROGOPTS[@]:1}")
+    done
+    if [[ ${#PROGARGS[@]} -ge 1 ]] ; then
+        ID="${PROGARGS[0]}"
     fi
-    [[ ${#PROGOPTS[@]} -eq 0 ]] || usage
-    for INDEX in $(index_list "${MIXTAPE_DIR}" "${SEARCH}") ; do
-        echo -n "${COLOR_WARN}$(index_epoch ${INDEX})${COLOR_RESET}: ${INDEX}"
-        xzcat ${INDEX} | wc -l | awk '{printf " (%s entries",$1}'
-        du -h ${INDEX} | awk '{printf ", %s)\n",$1}'
+    if [[ ${#PROGARGS[@]} -ge 2 ]] ; then
+        PREFIX="${PROGARGS[1]}"
+        if [[ "${PREFIX:0:1}" != "/" ]] ; then
+            PREFIX="/${PREFIX}"
+        fi
+    fi
+    for INDEX in $(index_list "${MIXTAPE_DIR}" "${ID}") ; do
+        ${FIRST} || [[ -z "${PREFIX}" ]] || echo
+        index_info "${INDEX}"
+        if [[ -n "${PREFIX}" ]] ; then
+            index_content "${INDEX}" "^${PREFIX}" | index_content_print ${FORMAT}
+        fi
+        FIRST=false
     done
 }
 
