31900e3726c9577652890a4fcfd87828d4d22e5c
Per Cederberg <per@percederberg.net>
2017-01-20 11:47:48 +0000

    status: Fix output when no data files exist

--- bin/mixtape-status.sh
+++ bin/mixtape-status.sh
@@ -12,40 +12,54 @@ source ${LIBRARY} || exit 1
 
 # Print mixtape status
 print_mixtape_status() {
-    local DIR=$1
+    local DIR=$1 FILES
     echo "${COLOR_WARN}--- Statistics for ${DIR}: ---${COLOR_RESET}"
-    cd ${DIR}/index 2>/dev/null || true
-    local INDEX_FILES=(*.xz)
-    if [[ ! -e ${INDEX_FILES[0]} ]] ; then
+    FILES=(${DIR}/index/*.xz)
+    if [[ ! -e ${FILES[0]} ]] ; then
         echo "No index files in ${DIR}/index/"
         echo
         return
     fi
     echo -n "Date range:     "
-    index_datetime ${INDEX_FILES[0]}
+    index_datetime ${FILES[0]}
     echo -n " ("
-    index_epoch ${INDEX_FILES[0]}
+    index_epoch ${FILES[0]}
     echo -n ") -- "
-    index_datetime ${INDEX_FILES[-1]}
+    index_datetime ${FILES[-1]}
     echo -n " ("
-    index_epoch ${INDEX_FILES[-1]}
+    index_epoch ${FILES[-1]}
     echo ")"
     echo -n "Indices:        "
     du -h --max-depth=0 ${DIR}/index | awk '{printf "%s",$1}'
-    echo -n ", ${#INDEX_FILES[@]} files, "
+    echo -n ", ${#FILES[@]} files, "
     xz --robot --list ${DIR}/index/*.xz | tail -1 | \
         awk '{printf  "ratio %.3f, %.2f MB saved\n", $6, ($5-$4)/1048576}'
     echo -n "Small files:    "
-    du -h --max-depth=0 ${DIR}/data/files | awk '{printf "%s, ",$1}'
-    find ${DIR}/data/files -type f | xargs -n 1 tar -t --absolute-names -f | \
-        wc -l | awk '{printf "%s files, ",$1}'
-    xz --robot --list ${DIR}/data/files/*/*.xz | tail -1 | \
-        awk '{printf  "%d archives, ratio %.3f, %.2f MB saved\n", $2, $6, ($5-$4)/1048576}'
+    FILES=(${DIR}/data/files/*/*.xz)
+    if [[ -e ${FILES[0]} ]] ; then
+        du -h --max-depth=0 ${DIR}/data/files | awk '{printf "%s, ",$1}'
+        find ${DIR}/data/files -type f | xargs -n 1 tar -t --absolute-names -f | \
+            wc -l | awk '{printf "%s files, ",$1}'
+        xz --robot --list ${DIR}/data/files/*/*.xz | tail -1 | \
+            awk '{printf  "%d archives, ratio %.3f, %.2f MB saved\n", $2, $6, ($5-$4)/1048576}'
+    else
+        printf "0K, 0 files\n"
+    fi
     echo -n "Large files:    "
-    du -h --max-depth=0 --exclude 'files/*' ${DIR}/data | awk '{printf "%s, ",$1}'
-    find ${DIR}/data/???/ -type f | wc -l | awk '{printf "%s files, ",$1}'
-    xz --robot --list ${DIR}/data/???/???/*.xz | tail -1 | \
-        awk '{printf  "%d compressed, ratio %.3f, %.2f MB saved\n", $2, $6, ($5-$4)/1048576}'
+    FILES=(${DIR}/data/???/???/*)
+    if [[ -e ${FILES[0]} ]] ; then
+        du -h --max-depth=0 --exclude 'files/*' ${DIR}/data | awk '{printf "%s, ",$1}'
+        find ${DIR}/data/???/ -type f | wc -l | awk '{printf "%s files, ",$1}'
+        FILES=(${DIR}/data/???/???/*.xz)
+        if [[ -e ${FILES[0]} ]] ; then
+            xz --robot --list ${DIR}/data/???/???/*.xz | tail -1 | \
+                awk '{printf  "%d compressed, ratio %.3f, %.2f MB saved\n", $2, $6, ($5-$4)/1048576}'
+        else
+            printf "0 compressed\n"
+        fi
+    else
+        printf "0K, 0 files\n"
+    fi
     echo
 }
 
