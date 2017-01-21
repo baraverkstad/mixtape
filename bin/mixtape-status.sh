dce6abb876fd4d96e7830f4919ecfda3b554f58e
Per Cederberg <per@percederberg.net>
2017-01-21 09:14:08 +0000

    status: Initial version of index status printing.

--- bin/mixtape-status.sh
+++ bin/mixtape-status.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 #
-# Prints a backup status summary.
+# Prints a backup status summary or a summary for a specified index.
 #
-# Syntax: mixtape-status
+# Syntax: mixtape-status [index]
+#
+# Arguments:
+#   <index>          The optional index id (e.g. "@586efbc4"), named search
+#                    (e.g. "first", "last") or partial timestamp (e.g.
+#                    "2017-01") with optional glob matching (e.g. "20??-*-01").
 #
 
 # Import common functions
@@ -10,7 +15,7 @@ SCRIPT=$(readlink $0 || echo -n $0)
 LIBRARY=$(dirname ${SCRIPT})/mixtape-common.sh
 source ${LIBRARY} || exit 1
 
-# Print mixtape status
+# Print mixtape directory stats summary
 print_mixtape_status() {
     local DIR=$1 FILES
     echo "${COLOR_WARN}--- Statistics for ${DIR}: ---${COLOR_RESET}"
@@ -72,20 +77,49 @@ print_disk_usage() {
     df -h ${DIR}
 }
 
+# Prints index content summary
+print_index_status() {
+    local INDEX_FILE=$1
+    echo "${COLOR_WARN}--- ${INDEX_FILE}: ---${COLOR_RESET}"
+    echo -n "Date:     "
+    index_datetime ${INDEX_FILE}
+    echo -n " ("
+    index_epoch ${INDEX_FILE}
+    echo ")"
+    echo -n "Size:     "
+    xz --robot --list ${INDEX_FILE} | tail -1 | \
+        awk '{printf  "%.1f KB compressed, ratio %.3f, %.1f KB saved\n", $4/1024, $6, ($5-$4)/1024}'
+    echo -n "Files:    "
+    xzcat ${INDEX_FILE} | wc -l | awk '{printf "%s total, ",$1}'
+    xzcat ${INDEX_FILE} | grep '^d' | wc -l | awk '{printf "%s dirs, ",$1}'
+    xzcat ${INDEX_FILE} | grep '^l' | wc -l | awk '{printf "%s symlinks, ",$1}'
+    xzcat ${INDEX_FILE} | grep '^-' | wc -l | awk '{printf "%s regular\n",$1}'
+    echo -n "Storage:  "
+    xzcat ${INDEX_FILE} | grep '^-' | cut -f 8 | grep ^files/ | wc -l | awk '{printf "%s smaller files (in ",$1}'
+    xzcat ${INDEX_FILE} | grep '^-' | cut -f 8 | grep ^files/ | sort | uniq | wc -l | awk '{printf "%s tar files), ",$1}'
+    xzcat ${INDEX_FILE} | grep '^-' | cut -f 8 | grep -v ^files/ | wc -l | awk '{printf "%s larger files\n",$1}'
+}
+
 # Program start
 main() {
+    local INDEX_FILE
     checkopts
-    [[ ${#ARGS[@]} -eq 0 ]] || usage "too many arguments"
-    if [[ ${MIXTAPE_DIR} != ${DEFAULT_MIXTAPE_DIR} ]] ; then
+    [[ ${#ARGS[@]} -le 1 ]] || usage "too many arguments"
+    if [[ ${#ARGS[@]} -eq 1 ]] ; then
+        for INDEX_FILE in $(index_files "${MIXTAPE_DIR}" "${ARGS[0]}") ; do
+            print_index_status ${INDEX_FILE}
+        done
+    elif [[ ${MIXTAPE_DIR} != ${DEFAULT_MIXTAPE_DIR} ]] ; then
         print_mixtape_status ${MIXTAPE_DIR}
+        print_disk_usage ${BACKUP_DIR}
     else
         for DIR in ${BACKUP_DIR}/*/mixtape ; do
             if is_mixtape_dir ${DIR} ; then
                 print_mixtape_status ${DIR}
             fi
         done
+        print_disk_usage ${BACKUP_DIR}
     fi
-    print_disk_usage ${BACKUP_DIR}
 }
 
 main
