1da3e10780dddd59daf91d7bb39cd2cc106ef4cc
Per Cederberg <per@percederberg.net>
2017-02-12 09:32:22 +0000

    mysql-dump: Code styles fixes for shellcheck

--- bin/mixtape-mysql-dump.sh
+++ bin/mixtape-mysql-dump.sh
@@ -12,25 +12,25 @@
 #
 
 # Import common functions
-SCRIPT=$(readlink $0 || echo -n $0)
-LIBRARY=$(dirname ${SCRIPT})/mixtape-common.sh
-source ${LIBRARY} || exit 1
+SCRIPT=$(readlink "$0" || echo -n "$0")
+LIBRARY=$(dirname "${SCRIPT}")/mixtape-common.sh
+source "${LIBRARY}" || exit 1
 
 # Global vars
-OPTIONS="--opt --quote-names --skip-add-locks --skip-lock-tables"
-EXTRAS=""
+OPTIONS=(--opt --quote-names --skip-add-locks --skip-lock-tables)
+EXTRAS=()
 
 # Handle command-line arguments
 parseargs "$@"
 [[ ${#ARGS[@]} -gt 0 ]] || usage "too few arguments"
 if [[ ${#OPTS[@]} -gt 0 ]] ; then
-    EXTRAS="${OPTS[@]}"
+    EXTRAS=("${OPTS[@]}")
 fi
 
 # Special Debian/Ubuntu default file location
 if [[ -r /etc/mysql/debian.cnf ]] ; then
-    OPTIONS="--defaults-extra-file=/etc/mysql/debian.cnf ${OPTIONS}"
+    OPTIONS=(--defaults-extra-file=/etc/mysql/debian.cnf "${OPTIONS[@]}")
 fi
 
 # Perform MySQL backup
-exec mysqldump ${OPTIONS} ${EXTRAS} --databases "${ARGS[@]}"
+exec mysqldump "${OPTIONS[@]}" "${EXTRAS[@]+"${EXTRAS[@]}"}" --databases "${ARGS[@]}"
