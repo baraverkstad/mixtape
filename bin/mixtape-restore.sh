c9b3d79c23e227d5c0bad2bf5c6d074626bcc5fa
Per Cederberg <per@percederberg.net>
2017-03-12 09:59:00 +0000

    restore: Added support for --debug and --quiet

--- bin/mixtape-restore.sh
+++ bin/mixtape-restore.sh
@@ -11,6 +11,14 @@
 #                    Use "/" to restore all files. An initial "/" char will be
 #                    added if missing.
 #
+# Options:
+#   --debug           Enables more output (verbose mode)
+#   --quiet           Disables normal output (quiet mode)
+#   --backup-dir=...  Use other root backup dir, instead of /backup
+#   --mixtape-dir=... Use other mixtape dir, instead of /backup/<host>/mixtape
+#   --help            Prints help information (and quits)
+#   --version         Prints version information (and quits)
+#
 
 # Import common functions
 SCRIPT=$(readlink "$0" || echo -n "$0")
@@ -27,12 +35,15 @@ restore_files() {
         if [[ ${ACCESS:0:1} == "d" && ! -e "${FILE:1}" ]] ; then
             mkdir -p "${FILE:1}"
         elif [[ ${ACCESS:0:1} == "l" ]] ; then
+            debug "restoring link: ${FILE}"
             ln -s "${LOCATION}" "${FILE:1}"
         elif [[ ${ACCESS:0:1} == "-" ]] ; then
             if [[ ${LOCATION:0:6} != "files/" ]] ; then
+                debug "restoring large file: ${FILE}"
                 largefile_restore "${MIXTAPE_DIR}/data/${LOCATION}" "${FILE:1}"
             elif [[ "${TARFILE}" != "${LOCATION}" ]] ; then
                 if [[ -n "${TARFILE}" ]] ; then
+                    debug "restoring from archive: ${TARFILE}"
                     tar -xf "${MIXTAPE_DIR}/data/${TARFILE}" -T "${FILES}"
                 fi
                 TARFILE="${LOCATION}"
@@ -43,6 +54,7 @@ restore_files() {
         fi
     done
     if [[ -n "${TARFILE}" ]] ; then
+        debug "restoring from archive: ${TARFILE}"
         tar -xf "${MIXTAPE_DIR}/data/${TARFILE}" -T "${FILES}"
     fi
 }
@@ -53,6 +65,7 @@ restore_meta() {
     while IFS=$'\t' read -r _ ACCESS USER GROUP DATETIME _ FILE _ _ ; do
         DST="${DIR}/${FILE:1}"
         if [[ -e "${DST}" ]] ; then
+            debug "restoring metadata: ${FILE}"
             chmod "$(file_access_octal "${ACCESS}")" "${DST}"
             chown "${USER}:${GROUP}" "${DST}"
             touch --no-dereference --date="${DATETIME}" "${DST}"
@@ -78,10 +91,10 @@ main() {
     fi
     INDEX_FILE=${ARR[0]}
     DIR=${MIXTAPE_DIR}/restore-$(index_datetime "${INDEX_FILE}" file)
-    echo -n "${COLOR_WARN}"
-    echo "Restoring ${FILEGLOB}"
-    echo "     from ${INDEX_FILE}"
-    echo "       to ${DIR}${COLOR_RESET}"
+    info "${COLOR_WARN}Backup dir:${COLOR_RESET}    ${MIXTAPE_DIR}"
+    info "${COLOR_WARN}Source index:${COLOR_RESET}  ${INDEX_FILE}"
+    info "${COLOR_WARN}Restore dir:${COLOR_RESET}   ${DIR}"
+    info "${COLOR_WARN}File path:${COLOR_RESET}     ${FILEGLOB}"
     index_content "${MIXTAPE_DIR}" "${INDEX_FILE}" "${FILEGLOB}" | \
         sort --field-separator=$'\t' --key=9,9 --key=7,7 | \
         restore_files "${DIR}"
